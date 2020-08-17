ee66c2deabd302f6ec616de9f3597bb2b4995e12
Mark <mark@dnamicro.com>
2020-08-17 08:16:36 +0800

    Add new library 'uuid' and export helper func

--- deps.ts
+++ deps.ts
@@ -1,10 +1,13 @@
 import "https://deno.land/x/dotenv/load.ts";
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 const PORT = Deno.env.get("PORT") || 3000;
+const generateID = v4.generate;
 
 export {
   PORT,
+  generateID,
 };
 
 export default Application;
