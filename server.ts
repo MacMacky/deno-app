66f47e50ecef0ab14b6f25788957f916c2128f46
Mark <mark@dnamicro.com>
2020-08-19 13:56:05 +0800

    Add new table 'user'

--- server.ts
+++ server.ts
@@ -2,5 +2,5 @@ import app from "./app.ts";
 import createDbAndTables from "./db/main.ts";
 import { DB } from "./helpers/index.ts";
 
-await createDbAndTables(DB, ["company"]);
+await createDbAndTables(DB, ["company", "user"]);
 await app.listen(`0.0.0.0:${Deno.env.get("PORT") || 3000}`);
