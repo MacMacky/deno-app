aea62ae6208556fe13fcc7f8075e53e2cca564b0
Mark <mark@dnamicro.com>
2020-08-19 12:28:53 +0800

    Modify 'deps' file export all need dependencies

--- deps.ts
+++ deps.ts
@@ -1,28 +1,14 @@
+// For loading the configuration
 import "https://deno.land/x/dotenv/load.ts";
-import {
-  Application,
-  RouterContext as Context,
-} from "https://deno.land/x/oak/mod.ts";
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
-const PORT = Deno.env.get("PORT") || 3000;
-const generateID = v4.generate;
-
-interface Company {
-  id: string;
-  name: string;
-  address: string;
-  description?: string;
-}
-
-type CompanyRequestBody = Pick<Company, "address" | "description" | "name">;
-
+// Export needed modules
 export {
-  PORT,
-  generateID,
-  Context,
-  Company,
-  CompanyRequestBody,
-};
-
-export default Application;
+  Application,
+  RouterContext,
+  Router,
+} from "https://deno.land/x/oak/mod.ts";
+export {
+  connect,
+  r,
+  Session,
+} from "https://deno.land/x/rethinkdb@0.1.0/mod.ts";
